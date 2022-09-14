4e6ba6dfc80ddb8ad6339938ae131cba3c0a3c95
Viktor <gexar47@gmail.com>
2022-09-14 03:22:18 +0300

    good

--- addpopupscriptfromcsv.py
+++ addpopupscriptfromcsv.py
@@ -1,6 +1,29 @@
 import csv
 
-with open('popuplist.csv', 'r', newline='') as csvfile:
-    reader = csv.reader(csvfile, delimiter=';', quoting=csv.QUOTE_MINIMAL)
-    for row in reader:
-        print(row[0]+' '+row[3]+' '+row[4]+' '+row[5]+' '+row[6]+' '+row[1])
+def atr_change(request):
+    with open('addpopup/popuplist.csv', 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+        for row in reader:
+            if (row[1]):#указать уникальное поле таблицы, по которому искать в БД
+                csvslug = row[1][:-1]
+                st = csvslug.split('/')
+                s = st[len(st)-1]
+                category = Category.objects.all()
+                for el in category:
+                    if el.slug == s:#тут сравнение полей
+                        try:
+                            article = Article.objects.get(category=el)
+                            #тут указать какие поля и на что меняются
+                            article.buttonurlpopup = row[0]
+                            article.buttonidpopup = 'someId'
+                            article.save()
+                            
+                        except:
+                            continue
+    return redirect("/moscow/", permanent=True)
+
+urls.py
+
+from .views import category_view, redirect_to_city, atr_change
+
+    url(r'moscow/s/', atr_change, name="atr_change"),
